c2b3e2aaee7d8e31b491eff95e0687dc122ce3be
Gayathiri Varsha A S <79994127+gayathiri-varsha@users.noreply.github.com>
2026-08-19 11:47:31 +0530

    Add: majority-element - submission-3

---
+++ Data Structures & Algorithms/majority-element/submission-3.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int res=0,max=0;
+        for(int i:nums){
+            int count = map.merge(i,1,Integer::sum);
+            if(count>max){
+                max=count;
+                res=i;
+            }
+        }
+        System.out.println(map);
+        // int[] count = new int[Integer.MAX_VALUE];
+        // int max=0,res=0;
+        // for(int i:nums){
+        //     count[i]++;
+        //     if(count[i]>max){
+        //         max=count[i];
+        //         res=i;
+        //     }
+        // }
+        return res;
+    }
+}
